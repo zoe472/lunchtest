e988352dec00a5fe7d065da8612e8a7c98ad9835
zoe472 <58350923+zoe472@users.noreply.github.com>
2023-03-21 23:31:48 +0900

    1

--- index.js
+++ index.js
@@ -51,4 +51,8 @@ app.get('/', (req, res) => {
     )
   });
     
-app.listen(3000);
+//app.listen(3000);
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log("listening server")
+})
